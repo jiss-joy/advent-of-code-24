53765d26a668c1f725a6e5dfbdcdfe61ef264589
Jiss Joy <60382278+jiss-joy@users.noreply.github.com>
2024-12-02 00:27:31 +0900

    part 2

--- day1/day1.js
+++ day1/day1.js
@@ -3,21 +3,38 @@ const path = require("path");
 
 let col1 = [];
 let col2 = [];
-let totalDistance = 0;
 
-function calculateDistance(rows) {
+function separateColumns(rows) {
   for (let row of rows) {
     const splitRow = row.split("   ");
     col1.push(splitRow[0]);
     col2.push(splitRow[1]);
   }
+}
+
+function calculateDistance() {
+  let totalDistance = 0;
   const sortedCol1 = col1.sort();
   const sortedCol2 = col2.sort();
 
   for (let i = 0; i < sortedCol1.length; i++) {
     totalDistance += Math.abs(sortedCol1[i] - sortedCol2[i]);
   }
-  console.log(totalDistance);
+  console.log('Distance: ', totalDistance);
+}
+
+function calculateSimilarityScore() {
+  let similarityScore = 0;
+  
+  for(let i = 0; i < col1.length; i++) {
+    let score = 0;
+    let left = col1[i];
+    for(let j = 0; j < col2.length; j++) {
+      if(left === col2[j]) score++;
+    }
+    similarityScore += left * score;
+  }
+  console.log('Similarity Score: ', similarityScore);
 }
 
 // ========
@@ -30,5 +47,7 @@ fs.readFile(inputFilePath, "utf8", (err, data) => {
     return;
   }
   const rows = data.trim().split("\n");
+  separateColumns(rows);
   calculateDistance(rows);
+  calculateSimilarityScore(rows);
 });
